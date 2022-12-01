54bb6d6ed511fd14b3e8cf41fa7103eed3d9e036
Paidamata <113390019+Paidamata@users.noreply.github.com>
2022-12-01 16:38:22 -0300

    cardCorreto

---
+++ Advance7Card - Copia/sites/main.js
@@ -0,0 +1,24 @@
+/*var c = 1*/
+
+/*do {
+   console.log(`Voce acabou de dar ${c} passo(s) voce é um vencedor`)
+   c++
+} while (c <= 600)*/
+//---------------------------
+for (var c = 1;c <=5;c++) {
+ conole.log(c)
+
+}
+
+
+
+
+/*var c = 1
+
+while (c <= 600) {
+   console.log(`Pass${c}`)
+   c++
+
+}*/
+
+
